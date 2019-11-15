261b512c395c3b2452f2c8b650d3a9e644b25273
hanli <hanli@maxthon.net>
2019-11-15 14:46:53 +0800

    修改MainActivity注释

--- app/src/main/java/com/worldtech/readnovel/MainActivity.java
+++ app/src/main/java/com/worldtech/readnovel/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //begin read novel
+        //begin read novel gogogo
 
 
 
